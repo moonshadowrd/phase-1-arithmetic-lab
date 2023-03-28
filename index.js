2eab62658eb33ceca7058d6ce4a0ad8538cd032a
jose cabreja <cabreja@yahoo.com>
2023-03-28 12:04:41 -0400

    creates myfile.md

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply); // Output: 62
 
-// Write your code here
+const random = Math.floor(Math.random() * 100) + 1; // Generates a random integer between 1 and 100
+console.log(random);
+
+const num3 = 4;
+const num4 = 16;
+const mod = num3 % num4;
+console.log(mod); // Output: 4
+
+const setOfNumbers = [5, 10, 20, 7, 3];
+const max = Math.max(...setOfNumbers);
+console.log(max); // Output: 20
